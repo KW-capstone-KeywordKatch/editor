17e8c099d037dfbf993502ba4ff838b24088a1e1
Mingeun Park <mindal99546@gmail.com>
2023-03-27 00:56:13 +0900

    feat: 모든 구두점 제거, 토큰화 기능 추가

--- core/analyzer/analyzer.py
+++ core/analyzer/analyzer.py
@@ -4,38 +4,14 @@
 import os
 import sys
 import re
+import time
 
-ARTICLE_ARCHIVE_PATH = '/Users/mingeun/KeywordKatch/core/article-crawler/articles/'
+ARTICLE_ARCHIVE_PATH = '/Users/mingeun/KeywordKatch/articles/'
 FREQUENT_SPECIAL_CHARS = ['▲', '=']
-PRESS = 'chosun'
-
-############################ 외부 라이브러리 사용 함수############################
-
 
 ################################# 자체 제작 함수 #################################
 
-def investigate(tokens):
-    '''
-    개발할 때 사용하는 함수.
-    토큰 길이별 개수 출력   
-    '''
-    max_len = len(tokens[0])
-    min_len = len(tokens[0])
-    for token in tokens:
-        token_len = len(token)
-        if max_len < token_len:
-            max_len = token_len
-        if min_len > token_len:
-            min_len = token_len
-    frequencies = [0]*(max_len+1)
-    total_count = len(tokens)
-    for token in tokens:
-        frequencies[len(token)] += 1
-    for i, n in enumerate(frequencies):
-        print("%2d-length token: %5d (%.2f%%)" % (i, n, (n/total_count)*100))
-
-
-def read_article(press, file_path):
+def read_article(file_path):
     '''
     전달 받은 경로의 기사를 메모리로 읽어 본문을 문자열 형태로 반환한다.
     '''
@@ -49,11 +25,8 @@ def get_paths_to_article(press):
     해당 언론사의 모든 기사 파일 제목(절대 경로 아님)을 리스트 형태로 반환한다.
     press - 언론사 이름
     '''
-    files  = os.listdir(ARTICLE_ARCHIVE_PATH + press)
-    paths = []
-    for title in files:
-        if title[0] != '.':
-            paths.append(ARTICLE_ARCHIVE_PATH + press + '/' + title)
+    paths  = [ARTICLE_ARCHIVE_PATH + press + '/' + file \
+            for file in os.listdir(ARTICLE_ARCHIVE_PATH + press) if file[0]!='.']
     return paths
 
 
@@ -61,8 +34,8 @@ def refine(content):
     '''
     의미 없는 기호(탈출문자, 구두점 등)를 제거한다.
     '''
-    content = re.sub('[\n()=]', ' ', content)            # 줄바꿈 공백으로 변환
-    content = re.sub('[\[\]\'\"▲]', '', content)     # 대괄호, 큰따옴표, 작은따옴표 제거
+    content = re.sub('[\n()=]', ' ', content)           # 줄바꿈, 괄호 공백으로 변환
+    content = re.sub('[\[\]"“‘”’▲,.\']', '', content)       # 대괄호, 큰따옴표, 작은따옴표 제거
     return content
 
 
@@ -88,20 +61,64 @@ def tokenize(content):
     return content
 
 
-def Main(press):
+def get_presses():
     '''
-    해당 언론사의 모든 기사에서 키워드를 추출
+    언론사 목록 획득
     '''
-    articles = get_paths_to_article(press)
-    total_token = []
-    for title in articles:
-        body = read_article(PRESS, title)
-        total_token.extend(tokenize(body))
-    # print(total_token)
-    investigate(total_token)
-    print(f'토큰 개수: {len(total_token)}') 
+    result = [press for press in os.listdir(ARTICLE_ARCHIVE_PATH) if press[0] != '.']
+    return result
 
+
+######################## for dev ###############################
+
+def print_distribution(tokens):
+    '''
+    토큰 길이별 개수 출력   
+    '''
+    max_len = len(tokens[0])
+    min_len = len(tokens[0])
+    for token in tokens:
+        token_len = len(token)
+        if max_len < token_len:
+            max_len = token_len
+        if min_len > token_len:
+            min_len = token_len
+    frequencies = [0]*(max_len+1)
+    total_count = len(tokens)
+    for token in tokens:
+        frequencies[len(token)] += 1
+    for i, n in enumerate(frequencies):
+        print("%2d-length token: %5d (%.2f%%)" % (i, n, (n/total_count)*100))
+
+def print_n_tokens(tokens, n):
+    '''
+    길이가 n인 모든 토큰 출력
+    '''
+    for token in tokens:
+        if len(token) == n:
+            print(token)
+    print()
+
+############################# main #############################
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        PRESS = sys.argv[1]
-    Main(PRESS)
+    presses = get_presses()
+    tokens = []
+    count_article = 0
+    start_time = time.time()        # [dev] 
+    for press in presses:
+        paths_to_article = get_paths_to_article(press)
+        for path in paths_to_article:
+            try:
+                body = read_article(path)
+                tokens += tokenize(body)
+                count_article += 1
+            except Exception as error:
+                print(f'[{error}]: {path}')
+    end_time = time.time()          # [dev]
+    # print
+    print_n_tokens(tokens, 3)
+    print(f"total token: {len(tokens)}")
+    print("elapsed time: %0.2f" % (end_time - start_time))
+    print(f"analyze {count_article} articles.")
+
+
