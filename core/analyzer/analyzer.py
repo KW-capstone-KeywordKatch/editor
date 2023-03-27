9cdffa9698bb19d4fa967b88bb2b102cdecdffd5
Mingeun Park <mindal99546@gmail.com>
2023-03-28 02:00:42 +0900

    feat: 각 문서에서 비슷한 토큰의 개수를 세는 기능

--- core/analyzer/analyzer.py
+++ core/analyzer/analyzer.py
@@ -34,8 +34,10 @@ def refine(content):
     '''
     의미 없는 기호(탈출문자, 구두점 등)를 제거한다.
     '''
-    content = re.sub('[\n()=]', ' ', content)           # 줄바꿈, 괄호 공백으로 변환
-    content = re.sub('[\[\]"“‘”’▲,.\']', '', content)       # 대괄호, 큰따옴표, 작은따옴표 제거
+    # 줄바꿈, 괄호 공백으로 변환
+    content = re.sub('[\t\n()=.]', ' ', content)           
+    # 대괄호, 큰따옴표, 작은따옴표 제거
+    content = re.sub('[\[\]"“‘”’▲△▷,\']', '', content)       
     return content
 
 
@@ -55,10 +57,10 @@ def tokenize(content):
     '''
     전달받은 문자열을 이루는 단어가 원소인 리스트를 반환한다.
     '''
-    content = refine(content)
-    content = [word  for word in content.split(' ') if len(word)>0]
+    tokens = refine(content)
+    tokens = [word  for word in tokens.split(' ') if len(word)>0]
 
-    return content
+    return tokens
 
 
 def get_presses():
@@ -67,6 +69,115 @@ def get_presses():
     '''
     result = [press for press in os.listdir(ARTICLE_ARCHIVE_PATH) if press[0] != '.']
     return result
+
+
+def get_n_tokens(tokens, n):
+    '''
+    길이가 n인 토큰들로 이루어진 리스트를 반환한다.
+    '''
+    result = [token for token in tokens if len(token) == n]
+    return result
+
+
+def term_frequency(documents, n_min, n_max):
+    '''
+    전체 토큰 중 길이가 start이상이고 end보다 작은 토큰들의 빈도수가 기록된 
+    딕셔너리 리스트를 반환한다.
+    반환된 배열은 
+    [{token: f, token: f, ...}, {token: f, token: f, ...}, ...] 형태이다.
+    document_tokens - 원소가 하나의 문서에서 추출한 모든 토큰 리스트인 리스트 
+    n_min - 토큰 길이 최솟값 
+    n_max - 토큰 길이의 상한값 (excluded)
+    '''
+    result = []
+    for document in documents:
+        frequencies = {}
+        for token in document:
+            if n_min <= len(token) < n_max:
+                if token not in frequencies:
+                    frequencies[token] = 1
+                else:
+                    frequencies[token] += 1
+        result.append(frequencies)
+
+    return result
+
+
+def is_reducable(term1, term2):
+    '''
+    두 단어가 적당히 비슷한지 판단한다.
+    '''
+    s_term = term1 if len(term1) < len(term2) else term2
+    l_term = term2 if len(term1) < len(term2) else term1
+    common_part_length = 0
+    i = 0
+    while i < len(s_term) and s_term[i] == l_term[i]:
+        common_part_length += 1
+        i+=1
+    if common_part_length > 1 and common_part_length >= len(s_term)/3:
+        return True
+    else:
+        return False
+
+
+def get_common_str(term1, term2):
+    '''
+    두 단어에서 공통된 부분을 반환한다.
+    '''
+    s_term = term1 if len(term1) < len(term2) else term2
+    l_term = term2 if len(term1) < len(term2) else term1
+    common_str = ''
+    i = 0
+    while i < len(s_term) and s_term[i] == l_term[i]:
+        common_str += s_term[i]
+        i+=1
+    return common_str
+
+
+def reduce(term_frequencies):
+    '''
+    하나의 문서에서 적당히 비슷한 토큰을 하나로 합친다.
+    term_frequencies - 하나의 문서에 대한 term_frequencies 딕셔너리
+    {term: f, term: f, ...} 형태
+    '''
+    result = {}
+    reduced_keys = set()
+    terms = list(term_frequencies.keys())
+    for i, term in enumerate(terms):
+        if term not in reduced_keys:
+            for other in terms[i+1:]:
+                if is_reducable(term, other):
+                    new_key = get_common_str(term, other)
+                    if new_key in result:
+                        result[new_key] += 1
+                    else:
+                        result[new_key] = term_frequencies[term] + term_frequencies[other]
+                    reduced_keys.add(term)
+                    reduced_keys.add(other)
+    # 합쳐지지 않은 토큰들을 보존해야 TF.IDF에서 유효한 결과가 나온다.
+    # 희귀한 단어들을 돋보이게 만든다.
+    for term in terms:
+        if term not in reduced_keys:
+            result[term] = term_frequencies[term]
+    return result
+
+
+def tf_idf_score(total_tokens):
+    '''
+    {각 토큰: tf-idf 점수} 형태의 딕셔너리를 반환한다.
+    n_tokens - 길이가 n인 토큰들의 리스트
+    n - 토큰의 길이
+    '''
+    # 딕셔너리 초기화
+    scores = {}
+    for token in total_tokens:
+        if token not in scores:
+            scores[token] = 0
+        else:
+            scores[token] += 1
+
+
+    return scores
 
 
 ######################## for dev ###############################
@@ -99,10 +210,19 @@ def print_n_tokens(tokens, n):
             print(token)
     print()
 
+
+def print_token_score(score):
+    count = 0
+    for t, s in score.items():
+        if s>1:
+            count += 1
+            print("%-4s : %-5d" % (t, s))
+    print(f'2번 이상 등장한 토큰의 개수: {count}')
+
 ############################# main #############################
 if __name__ == "__main__":
     presses = get_presses()
-    tokens = []
+    documents_tokens = []
     count_article = 0
     start_time = time.time()        # [dev] 
     for press in presses:
@@ -110,14 +230,21 @@ if __name__ == "__main__":
         for path in paths_to_article:
             try:
                 body = read_article(path)
-                tokens += tokenize(body)
+                documents_tokens.append(tokenize(body))
                 count_article += 1
             except Exception as error:
                 print(f'[{error}]: {path}')
+    term_frequencies = term_frequency(documents_tokens, 3, 11)
+    for i, document in enumerate(term_frequencies):
+        term_frequencies[i] = reduce(reduce(document))
     end_time = time.time()          # [dev]
     # print
-    print_n_tokens(tokens, 3)
-    print(f"total token: {len(tokens)}")
+    total_token_count = 0
+    for i, document in enumerate(term_frequencies):
+        print('document%4d'%i)
+        print(document)
+        total_token_count += len(document)
+    print("기사 한 개당 평균 토큰 개수: %0.1f" % (total_token_count/len(term_frequencies)))
     print("elapsed time: %0.2f" % (end_time - start_time))
     print(f"analyze {count_article} articles.")
 
