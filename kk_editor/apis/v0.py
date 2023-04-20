053d9b61964b0c1ec48c951f1f0c280e638e91d2
Mingeun Park <mindal99546@gmail.com>
2023-04-20 23:42:28 +0900

    feat(prototype): 중간 발표 시연용 api 구현

--- kk_editor/apis/v0.py
+++ kk_editor/apis/v0.py
@@ -3,10 +3,11 @@
 """
 
 from flask_restx import Namespace, Resource, fields
-from flask import request
+from flask import request, jsonify
 from core.analyzer import analyzer
 from kk_editor import db
-from kk_editor.models import Article
+from kk_editor.models import Article, Keywords
+from sqlalchemy.exc import NoResultFound
 
 
 api = Namespace('v0', description='prototype operations')
@@ -17,6 +18,21 @@ api = Namespace('v0', description='prototype operations')
 class Recommend(Resource):
     @api.doc('기사 추천')
     def get(self, keyword):
-        analyzer.fetch_articles(db, Article)
-        return f'recommend for {keyword}'
+        try: 
+            data = analyzer.recommend_by_keyword(keyword, Keywords, Article)
+            response = {'code': 1,
+                        'payload': data}
+            return jsonify(response)
+        # 해당 검색어에 대한 기사가 없음
+        except NoResultFound as e:
+            response = {'code': 0,
+                        'payload': None}
+            return jsonify(response)
 
+
+@api.route('/analyze')
+class Analyze(Resource):
+    def get(self):
+        analyzer.extract_keyword(db, Article)
+        analyzer.collect_keyword(db, Keywords)
+        return 'analyze'
