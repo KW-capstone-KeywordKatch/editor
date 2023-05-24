3713d13761a69ecfc8fb317a1f89781efac75226
Mingeun Park <mindal99546@gmail.com>
2023-05-25 00:25:46 +0900

    chore(katch): 로컬 컨테이너 실행 명령어 추가 $./katch.py execute local

--- kk_editor/katch.py
+++ kk_editor/katch.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+
 """
 플라스크 애플리케이션이 동작하기 위한 환경 구성 및 빌드 스크립트
 """
@@ -6,19 +7,16 @@
 import os
 import sys
 import subprocess
-import pty
-import errno
-from time import sleep
 
-#################### Status ######################
-SYNC_DEP = False
-EXECUTE_APP = False
+'''------------------- Status -------------------'''
+SYNC_DEP = False            # 의존성(패키지) 설치
+EXECUTE_APP = False         # 앱 실행
 SETUP = True
 INTEGRATE = False
-##################################################
+CONTAINER = False
+PROD = True
 
-
-################## ANSI control ##################
+'''----------------- ANSI Control ----------------'''
 
 GREET_VENV = r"""
  __     ___      _               _                   _                                      _
@@ -36,6 +34,15 @@ GREET_APP = r"""
  | . \| . \  |_____| |  __/ (_| | | || (_) | |
  |_|\_\_|\_\          \___|\__,_|_|\__\___/|_|
 """
+
+GREET_DOCKER = r"""
+  ____             _
+ |  _ \  ___   ___| | _____ _ __
+ | | | |/ _ \ / __| |/ / _ \ '__|
+ | |_| | (_) | (__|   <  __/ |
+ |____/ \___/ \___|_|\_\___|_|
+"""
+
 
 # 한 줄을 비우고 커서를 맨 앞으로 이동
 def _clear_line(msg):
@@ -85,12 +92,21 @@ def _parse(argv):
     global SYNC_DEP
     global EXECUTE_APP
     global SETUP
+    global INTEGRATE
+    global PROD
+    global CONTAINER
     if 'setup' in argv:
         SETUP = True
     if 'sync' in argv:
         SYNC_DEP = True
     if 'execute' in argv:
         EXECUTE_APP = True
+    if 'integrate' in argv:
+        INTEGRATE = True
+    if 'local' in argv:
+        PROD = False
+    if 'container' in argv:
+        CONTAINER = True
 
 def main(argv):
     _parse(argv)
@@ -106,8 +122,30 @@ def main(argv):
     # 플라스크 애플리케이션 실행
     if EXECUTE_APP:
         _print_with_color('blue', GREET_APP, clear=True)
+        if CONTAINER:
+            _print_with_color('blue', GREET_DOCKER)
+        sys.stdout.flush()
+        # 환경변수 설정
         _set_environ()
-        _call("flask run", cwd=os.sep.join(os.getcwd().split(os.sep)[:-1]))
+        print(os.getcwd())
+        # container 내부에서 실행
+        if CONTAINER:
+            _call("flask run --host=0.0.0.0", cwd=os.sep.join(os.getcwd().split(os.sep)[:-1]))
+        else:
+            # 배포환경이 아닌 로컬 환경에서 실행
+            if not PROD:
+                _call("docker container run -v /Users/mingeun/logs:/app/kk_editor/logs -p 8000:8000 local/editor:latest")
+            # 배포환경에서 실행
+            else:
+                _call("flask run --host='0.0.0.0'", cwd=os.sep.join(os.getcwd().split(os.sep)[:-1]))
+    if INTEGRATE:
+        if PROD:
+            # 도커 이미지 빌드 -> 허브에 업로드
+            _call("docker image build --no-cache -t mingeun2154/editor:latest .")
+            _call("docker image push mingeun2154/editor:latest")
+        # local 
+        else:
+            _call("docker image build --no-cache -t local/editor:latest .")
 
 if __name__ == "__main__":
     try: 
